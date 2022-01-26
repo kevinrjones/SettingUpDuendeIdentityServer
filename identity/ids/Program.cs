b4e02d0332273732a6d28228c4faef1c09205009
Kevin <kevin@rocksolidknowledge.com>
2022-01-26 15:38:51 +0000

    Code for video

---
+++ identity/ids/Program.cs
@@ -0,0 +1,32 @@
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddIdentityServer(options =>
+  {
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+
+    options.EmitStaticAudienceClaim = true;
+  }).AddTestUsers(TestUsers.Users)
+  .AddInMemoryClients(Config.Clients)
+  .AddInMemoryApiResources(Config.ApiResources)
+  .AddInMemoryApiScopes(Config.ApiScopes)
+  .AddInMemoryIdentityResources(Config.IdentityResources);
+
+builder.Services.AddAuthentication();
+
+var app = builder.Build();
+
+app.UseIdentityServer();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages().RequireAuthorization();
+
+app.Run();
